8cd1c6fc279c384b31beb4113a154d77c0bb3518
stefley <32701979+stefley@users.noreply.github.com>
2022-02-23 11:47:16 +0800

    Update myBind.js

--- myBind.js
+++ myBind.js
@@ -1,13 +1,19 @@
-Function.prototype.myBInd = function(context) {
-    //返回一个绑定this的函数
-    let _this = this;
-    //可以支持柯里化传参，保存参数
-    let args = [...arguments].slice(1);
-    // 返回函数
-    return function() {
-        let newArgs = [...arguments];
-        //返回函数绑定this，传入两次保存的参数
-        //考虑返回函数有返回值，return
-        return _this.apply(context, args.concat(newArgs));
-    }
+/**
+ * bind的第一个参数用来改变this指向
+ * bind会返回一个新函数
+ * bind的传参方式和call相同，但是可以分离参数，可以将原函数的参数部分传入bind，剩下的参数传入返回的新函数
+ * 实例化返回的新函数，this指向原函数构造出来的实例
+ * 实例应该继承原函数的原型
+ */
+Function.prototype.myBind = function(ctx) {
+  const originFn = this;
+  let args = [].slice.call(arguments, 1)
+  let tempFn = function() {}
+  const newFn = function () {
+    let restArgs = [].slice.call(arguments)
+    return originFn.apply(this instanceof newFn ? this : ctx, args.concat(restArgs))
+  }
+  tempFn.prototype = this.prototype
+  newFn.prototype = new tempFn()
+  return newFn
 }
